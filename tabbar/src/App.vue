d4e9cbad962670776310cbc442e98868343632c5
xiaolu <994146765@qq.com>
2019-12-24 22:12:15 +0800

    tabbar开始

---
+++ tabbar/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <tabbar></tabbar>
+  </div>
+</template>
+
+<script>
+import Tabbar from './components/tabbar/Tabbar'
+export default {
+  name: 'App',
+  components:{
+    Tabbar
+  }
+}
+</script>
+
+<style>
+@import "./assets/css/base.css";
+#tab-bar {
+  display: flex;
+  background-color: #f6f6f6;
+
+/* 定位到底部 */
+  position: fixed;
+  left:0;
+  right: 0;
+  bottom: 0;
+
+/* 阴影 */
+box-shadow: 0 -1px 1px rgba(190, 115, 115, 0.1)
+
+}
+.tab-bar-item{
+  flex: 1;
+  text-align: center;
+  height: 49px;
+}
+</style>
---
+++ tabbar/src/components/tabbar/Tabbar.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <div id="tab-bar">
+      <div class="tab-bar-item">
+        <!-- <img src="../../assers/img/tabbar/1.jpg" alt=""> -->
+        首页
+        </div>
+      <div class="tab-bar-item">分类</div>
+      <div class="tab-bar-item">购物车</div>
+      <div class="tab-bar-item">我的</div>
+    </div>
+  
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Tabbar'
+}
+</script>
+
+<style>
+
+#tab-bar {
+  display: flex;
+  background-color: #f6f6f6;
+
+/* 定位到底部 */
+  position: fixed;
+  left:0;
+  right: 0;
+  bottom: 0;
+
+/* 阴影 */
+box-shadow: 0 -1px 1px rgba(190, 115, 115, 0.1)
+
+}
+.tab-bar-item{
+  flex: 1;
+  text-align: center;
+  height: 49px;
+}
+</style>
