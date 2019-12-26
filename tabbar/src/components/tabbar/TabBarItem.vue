e3e8216905a4fd46c541cdd7c128b3b35fb18a25
xiaolu <994146765@qq.com>
2019-12-26 23:35:31 +0800

    完成tabbar

--- tabbar/src/components/tabbar/TabBarItem.vue
+++ tabbar/src/components/tabbar/TabBarItem.vue
@@ -1,8 +1,16 @@
 <template>
   
-    <div class="tab-bar-item">
-      <slot name="item-icon"></slot>
-      <slot name="item-text"></slot>
+    <div class="tab-bar-item" @click="itemClick">
+      <div v-if="!isActive"><slot  name="item-icon"></slot></div>
+      <div  v-else><slot name="item-icon-active"></slot></div>
+      <!-- 此处包裹一个div的目的是防止slot被替换之后，active属性一起被替换，然后效果不生效 -->
+      <!-- <div :class="{active: isActive}">
+        <slot name="item-text"></slot>
+      </div> -->
+      <div :style="activeStyle">
+        <slot name="item-text"></slot>
+      </div>
+      
         <!-- <img src="../../assets/img/tabbar/home.svg" alt="">
         <div>首页</div> -->
         </div>
@@ -11,7 +19,36 @@
 
 <script>
 export default {
-  name:'TabBarItem'
+  name:'TabBarItem',
+  props:{
+    path: String,
+    activeColor:{
+      type:String,
+      default:'red'
+    }
+  },
+  data(){
+    return {
+      // 这个不能写死，因为需要点击哪个哪个变红
+      // isActive:true
+      
+    }
+  },
+  computed:{
+    isActive(){
+      // indexOf用法：有没有在前边找到后面，没找到返回-1
+      return this.$route.path.indexOf(this.path) !== -1
+    },
+    activeStyle() {
+      return this.isActive ? {color: this.activeColor} : {}
+    }
+  },
+  methods:{
+    itemClick() {
+      console.log('itemclick');
+      this.$router.push(this.path)
+    }
+  }
 }
 </script>
 
--- tabbar/src/router/index.js
+++ tabbar/src/router/index.js
@@ -1,15 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import VueRouter from 'vue-router'
+// 懒加载方式
+const Home =() => import('../views/home/Home')
+const Cart =() => import('../views/cart/Cart')
 Vue.use(Router)
-
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+const routes =[
+  {
+    path:'',
+    redirect:'/home'
+  },
+  {
+    path: '/home',
+    component: Home
+  },
+  {
+    path: '/cart',
+    component: Cart
+  }
+]
+const router = new VueRouter({
+  routes,
+  mode:'history'
 })
+
+export default router
